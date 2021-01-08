c8ad712b5fba5c64b329a663bdeb2fe8e5c83ff7
Anoop S Unnithan <anoopsunnithan@gmail.com>
2021-01-09 00:55:49 +0530

    Working on strings

--- Strings.py
+++ Strings.py
@@ -3,3 +3,13 @@
 myString = "Hello World!"
 print(myString)
 print(len(myString))
+
+#reversing a string
+print(myString[::-1])
+
+
+### String Properties ###
+
+# Immutablility
+# name = "Sam"
+# name[0] = 'p'
